2d8ee02093f73434e4d1f402a3a0cf0470de2beb
Antonio <tonoezep@gmail.com>
2020-03-16 16:48:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+import api from './utils/api';
 import './App.css';
+import {
+  List,
+  Avatar
+} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [{
+    title: 'Ant Design Title 1'
+  },
+  {
+    title: 'Ant Design Title 2'
+  },
+  {
+    title: 'Ant Design Title 3'
+  },
+  {
+    title: 'Ant Design Title 4'
+  }
+];
 
-export default App;
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoading: true,
+        name: null,
+        avatar: null,
+        email: null
+      };
+    }
+
+    render() {
+      const {
+        isLoading,
+        name,
+        avatar,
+        email
+      } = this.state;
+
+      return ( <
+        List itemLayout = "horizontal"
+        dataSource = {
+          data
+        }
+        renderItem = {
+          item => ( <
+            List.Item >
+            <
+            List.Item.Meta avatar = {
+              <
+              Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" / >
+            }
+            title = {
+              <
+              a href = "https://ant.design" > {
+                item.title
+              } < /a>}
+              description = "Ant Design, a design language for background applications, is refined by Ant UED Team" /
+              >
+              <
+              /List.Item>
+            )
+          }
+          />
+        );
+      }
+
+      async componentDidMount() {
+        // Load async data.
+        // Update state with new data.
+        // Re-render our component.
+      }
+    }
+
+    export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://gateway.marvel.com:443",
+  responseType: "json"
+});
